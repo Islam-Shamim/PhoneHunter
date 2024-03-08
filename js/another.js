14fcf40a5a1e344d65ed758a45847cdb4f06cf06
Islam-Shamim <islamshamim1819@gmail.com>
2024-03-08 12:17:08 +0600

    second

--- js/another.js
+++ js/another.js
@@ -1,28 +1,28 @@
-const loadPhone = async(searchText,isShowAll) =>{
+const loadPhone = async (searchText = '13', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhone(phones,isShowAll);
+    displayPhone(phones, isShowAll);
 }
-const displayPhone = (phones,isShowAll) => {
+const displayPhone = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
     //show all button
     const showAllContainer = document.getElementById('show-container');
-    if(phones.length > 12 && !isShowAll){
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     }
-    else{
+    else {
         showAllContainer.classList.add('hidden');
     }
 
     // display only first 12 if not show all button
-    if(!isShowAll){
-        phones = phones.slice(0,12);
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
     }
-    
 
-    phones.forEach(phone =>{
+
+    phones.forEach(phone => {
         //console.log(phone)
         //console.log('is show all',isShowAll);
         //create a div
@@ -31,10 +31,10 @@ const displayPhone = (phones,isShowAll) => {
         createDiv.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
             <div class="card-body">
-                <h2 class="card-title">${phone.brand}</h2>
-                <p>${phone.phone_name}</p>
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>A mobile phone is a portable telephone.</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-warning">Buy Now</button>
+                    <button onclick="showDetails('${phone.slug}')" class="btn btn-warning">Show Details</button>
                 </div>
             </div>
         `
@@ -43,29 +43,59 @@ const displayPhone = (phones,isShowAll) => {
     showSpinner(false);
 }
 
-//loadPhone();
+loadPhone();
 
 //eventHandler
-const clickHander2 = (isShowAll) =>{
+const clickHander2 = (isShowAll) => {
     showSpinner(true);
     const search = document.getElementById('phone-name2');
     const searchText = search.value;
-    loadPhone(searchText,isShowAll);
+    loadPhone(searchText, isShowAll);
     //search input text blank after searching
     //search.value = '';
 }
 //loading spinner
-const showSpinner = (isLoading) =>{
+const showSpinner = (isLoading) => {
     const loadSpinner = document.getElementById('load-spinner');
-    if(isLoading){
+    if (isLoading) {
         loadSpinner.classList.remove('hidden');
     }
-    else{
+    else {
         loadSpinner.classList.add('hidden');
     }
 }
 
 //handle show all 
-const handleShowAll = () =>{
+const handleShowAll = () => {
     clickHander2(true);
+}
+
+//show details
+const showDetails = async (id) => {
+    //console.log('Show details',id);
+    //load single data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    showPhoneDetails(data.data);
+
+}
+
+//show phone details
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+    const detailsPhone = document.getElementById('show-phone-details');
+    detailsPhone.innerHTML = `
+    <figure><img src="${phone.image}" alt="phone" /></figure>
+    <h3 class="font-bold text-lg">${phone.name}</h3>
+    <p class="py-4">Storage : ${phone.mainFeatures.storage}
+    <br>Display size : ${phone.mainFeatures.displaySize}<br>
+    Storage : ${phone.mainFeatures.memory}<br>Chipset : ${phone.mainFeatures.chipSet}</p>
+    <h4><span class="font-bold">Release Date:</span>  ${phone.releaseDate ? phone.releaseDate : "Unknown"} </h4>
+    <div class="modal-action">
+        <form method="dialog">
+                <button class="btn">Close</button>
+        </form>
+    </div>
+    `
+    show_details_modal.showModal();
 }
